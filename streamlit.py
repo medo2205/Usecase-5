82efea78099332132fe62d7bd31facb1593e8d4a
MohammedAlamri <m.s.alamri22@gmail.com>
2024-08-15 02:14:53 +0300

    test

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,14 @@
 import streamlit as st
+
+st.markdown("<h1 style='text-align: right; '>!هل الرياض الحل؟</h1><br>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: right; '>كحديث تخرج من سكان المدينة المنورة حاولت ابحث عن اكثر منطقة فيها وظائف</p><br>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: right; '>مالقيت اجابة لكن اخوي كان عنده الحل</h1><br>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: right; '>وقال لي ان عدد كبير من الوظئف متوفرة في الرياض</h1><br>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: right; '>لكن ماصدقت الين وضحلي بالرسم البياني هذا:</h1><br>", unsafe_allow_html=True)
+st.image("newplot.png")
+st.markdown("<h1 style='text-align: right; '>زي ماهو واضح الرياض هي اكثر منطقة فيها توفر في الوظائف </h1><br>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: right; '>بحكم اني حديث تخرج كنت شايل هم الرواتب لكن اخوي طمني قال عندي السكن لا تشيل هم وهذي حدود الرواتب للاشخاص اللي بدون خبرة:</h1><br>", unsafe_allow_html=True)
+st.image("newplot1.png")
+st.markdown("<h1 style='text-align: right; '>ونقدر نلاحظ ان التفاوت بين اعداد الوظائف للرجال و النساء خفيف</h1><br>", unsafe_allow_html=True)
+st.image("newplot2.png")
+
